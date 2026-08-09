5e3c4d61011878e342c9a067905e54ca17114874
曇 <necodev@icloud.com>
2026-08-09 13:43:16 +0800

    Dots

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".slide-dot");
+const hero = document.querySelector("#home-hero");
+
+dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+        slides.forEach((slide) => {
+            slide.classList.remove("active");
+        });
+
+        dots.forEach((dot) => {
+            dot.classList.remove("active");
+        });
+
+        slides[index].classList.add("active");
+        dot.classList.add("active");
+
+        hero.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+});
+
 function wonderhoyBaby() {
     wonderhoy.play();
 }
